5614467a4891e3959bdafaf043670ee84f383d05
Diogo Jhony <diogojhony@gmail.com>
2020-10-14 21:25:03 -0300

    chore: Work with views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,15 +1,19 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
+import orphanageView from '../views/orphanages_view';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -17,9 +21,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
@@ -36,9 +42,9 @@ export default {
     const orphanagesRepository = getRepository(Orphanage);
 
     const requestImages = request.files as Express.Multer.File[];
-    const images = requestImages.map(image => ({
-      path: image.filename
-    }))
+    const images = requestImages.map((image) => ({
+      path: image.filename,
+    }));
 
     const orphanage = orphanagesRepository.create({
       name,
@@ -48,7 +54,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
-      images
+      images,
     });
 
     await orphanagesRepository.save(orphanage);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 
 import './database/connection';
@@ -8,5 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);
